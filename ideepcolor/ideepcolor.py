b5b2cc9eb1e95fdca4472e79c0f04256adbbc645
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2021-10-05 22:45:52 +1100

    Remove dead code

--- ideepcolor/ideepcolor.py
+++ ideepcolor/ideepcolor.py
@@ -38,9 +38,8 @@ class ColorPoint:
         cv2.rectangle(im, tl, br, self.color, -1)
 
 class Draw:
-    def __init__(self, color_model, dist_model, load_size):
+    def __init__(self, color_model, load_size):
         self.model = color_model
-        self.dist_model = dist_model
         self.load_size = load_size
         self.points = []
 
@@ -55,39 +54,18 @@ class Draw:
         self.image_file = image_file
         print(image_file)
         im_bgr = cv2.imread(image_file)
-        self.im_full = im_bgr.copy()
-        # get image for display
-        h, w, c = self.im_full.shape
-
-        im_gray = cv2.cvtColor(im_bgr, cv2.COLOR_BGR2GRAY)
-        self.im_gray3 = cv2.cvtColor(im_gray, cv2.COLOR_GRAY2BGR)
-
         im_bgr = cv2.resize(im_bgr, (self.load_size, self.load_size), interpolation=cv2.INTER_CUBIC)
-        self.im_rgb = cv2.cvtColor(im_bgr, cv2.COLOR_BGR2RGB)
 
         self.im_lab = color.rgb2lab(im_bgr[:, :, ::-1])
         self.im_l = self.im_lab[:, :, 0]
         self.im_ab = self.im_lab[:, :, 1:]
-        self.im_size = self.im_rgb.shape[0:2]
+        self.im_size = im_bgr.shape[0:2]
 
         self.im_ab0 = np.zeros((2, self.load_size, self.load_size))
         self.im_mask0 = np.zeros((1, self.load_size, self.load_size))
         self.brushWidth = 2 * self.scale
 
         self.model.load_image(image_file)
-
-        if (self.dist_model is not None):
-            self.dist_model.set_image(self.im_rgb)
-
-    def predict_color(self):
-        if self.dist_model is not None and self.image_loaded:
-            im, mask = self.get_input()
-            im_mask0 = mask > 0.0
-            self.im_mask0 = im_mask0.transpose((2, 0, 1))
-            im_lab = color.rgb2lab(im).transpose((2, 0, 1))
-            self.im_ab0 = im_lab[1:3, :, :]
-
-            self.dist_model.net_forward(self.im_ab0, self.im_mask0)
 
     def get_input(self):
         h = self.load_size
@@ -114,8 +92,6 @@ class Draw:
         self.result = pred_rgb
 
     def save_result(self):
-        self.compute_result()
-
         path = os.path.abspath(self.image_file)
         path, ext = os.path.splitext(path)
 
@@ -126,17 +102,7 @@ class Draw:
         if not os.path.exists(save_path):
             os.mkdir(save_path)
 
-        #np.save(os.path.join(save_path, 'im_l.npy'), self.model.img_l)
-        #np.save(os.path.join(save_path, 'im_ab.npy'), self.im_ab0)
-        #np.save(os.path.join(save_path, 'im_mask.npy'), self.im_mask0)
-
-        #result_bgr = cv2.cvtColor(self.result, cv2.COLOR_RGB2BGR)
-        #mask = self.im_mask0.transpose((1, 2, 0)).astype(np.uint8) * 255
-        #cv2.imwrite(os.path.join(save_path, 'input_mask.png'), mask)
-        #cv2.imwrite(os.path.join(save_path, 'ours.png'), result_bgr)
         cv2.imwrite(os.path.join(save_path, 'ours_fullres.png'), self.model.get_img_fullres()[:, :, ::-1])
-        #cv2.imwrite(os.path.join(save_path, 'input_fullres.png'), self.model.get_input_img_fullres()[:, :, ::-1])
-        #cv2.imwrite(os.path.join(save_path, 'input.png'), self.model.get_input_img()[:, :, ::-1])
         cv2.imwrite(os.path.join(save_path, 'input_ab.png'), self.model.get_sup_img()[:, :, ::-1])
 
 ############### SETUP ###############
@@ -156,12 +122,8 @@ colorModel = CI.ColorizeImageTorch(Xd=load_size)
 colorModel.prep_net(path=color_model, gpu_id=gpu_id)
 print('')
 
-distModel = CI.ColorizeImageTorchDist(Xd=load_size)
-distModel.prep_net(path=color_model, gpu_id=gpu_id, dist=True)
-print('')
-
 print(f'Loading grayscale {test_image}')
-draw = Draw(colorModel, distModel, load_size)
+draw = Draw(colorModel, load_size)
 draw.read_image(test_image)
 print('')
 
@@ -169,5 +131,12 @@ print('Overlaying RGB')
 for y in range(h):
 	for x in range(w):
 		draw.add_point(ColorPoint([x, y], (int(rgb[y, x, 2]), int(rgb[y, x, 1]), int(rgb[y, x, 0])), 1))
+print('')
+
+print('Computing...')
+draw.compute_result()
+print('')
+
+print('Saving...')
 draw.save_result()
 
